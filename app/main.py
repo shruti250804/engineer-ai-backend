ea8136cf0e76064274527f814149d6ba1fac4b82
Shruti Rana <ranashurtu@gmail.com>
2026-08-13 00:06:42 +0530

    Add GET, PUT and DELETE repository endpoints

--- app/main.py
+++ app/main.py
@@ -1,5 +1,4 @@
-
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from .database import engine, SessionLocal, get_db
 from . import models
@@ -26,3 +25,75 @@ def create_repository(
 
     return db_repo
 
+@app.get("/repositories")
+def get_repositories(
+    db: Session = Depends(get_db)
+):
+    repositories = db.query(Repository).all()
+    return repositories
+
+
+# GET BY ID
+@app.get("/repositories/{repo_id}")
+def get_repository(
+    repo_id: int,
+    db: Session = Depends(get_db)
+):
+    repo = db.query(Repository).filter(
+        Repository.id == repo_id
+    ).first()
+
+    if repo is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Repository not found"
+        )
+
+    return repo
+
+@app.put("/repositories/{repo_id}")
+def update_repository(
+    repo_id: int,
+    repository: RepositoryCreate,
+    db: Session = Depends(get_db)
+):
+    repo = db.query(Repository).filter(
+        Repository.id == repo_id
+    ).first()
+
+    if repo is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Repository not found"
+        )
+
+    repo.name = repository.name
+    repo.github_url = repository.github_url
+    repo.description = repository.description
+
+    db.commit()
+    db.refresh(repo)
+
+    return repo
+
+@app.delete("/repositories/{repo_id}")
+def delete_repository(
+    repo_id: int,
+    db: Session = Depends(get_db)
+):
+    repo = db.query(Repository).filter(
+        Repository.id == repo_id
+    ).first()
+
+    if repo is None:
+        raise HTTPException(
+            status_code=404,
+            detail="Repository not found"
+        )
+
+    db.delete(repo)
+    db.commit()
+
+    return {
+        "message": "Repository deleted successfully"
+    }
